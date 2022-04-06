14312856df412c7e3e70a82c4bdf712ab8e32134
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-04-05 19:58:51 -0500

    Listar juegos 2

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
@@ -2,6 +2,7 @@ package pe.edu.pucp.gtics.lab1221.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.pucp.gtics.lab1221.entity.Games;
@@ -17,10 +18,10 @@ public class GamesController {
     GamesRepository gamesRepository;
 
     @GetMapping("/lista")
-    public String listaJuegos (){
+    public String listaJuegos (Model model){
         List<Games> listaJuegos = gamesRepository.findAll();
-
-        return "";
+        model.addAttribute("listaJuegos",listaJuegos);
+        return "juegos/lista";
     };
 
     public String editarJuegos(){
